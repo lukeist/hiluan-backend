a6c2287a24944916e1a8e9e563c64eb7c1b4a834
hiluan <apt.muenchen@gmail.com>
2022-03-18 23:33:06 -0700

    1. Working Contact Form API

---
+++ db/conn.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require("mongodb");
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(Db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+var _db;
+
+module.exports = {
+  connectToServer: (callback) => {
+    client.connect((err, db) => {
+      // Verify we got a good "db" object
+      if (db) {
+        _db = db.db("contacts");
+        console.log("Successfully connected to MongoDB.");
+      }
+      return callback(err);
+    });
+  },
+  getDb: () => {
+    return _db;
+  },
+};
